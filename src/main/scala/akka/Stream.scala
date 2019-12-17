e8bea454017fb2381e6322c5e658d0d79dda9825
doma91 <dominika.malinowska@sky.uk>
2019-12-17 12:43:05 +0000

    Updating code to reflect further investigations into Akka Streams

--- src/main/scala/akka/Stream.scala
+++ src/main/scala/akka/Stream.scala
@@ -8,51 +8,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.unmarshalling.Unmarshal
-
-import scala.util.{Failure, Success}
-
-
-object Stream extends App {
-
-  val url = "https://api.stripe.com/v1/balance_transactions?limit=3"
-  val accessToken = BasicHttpCredentials("sk_test_4eC39HqLyjWDarjtT1zdp7dc", "")
-  val request = HttpRequest(HttpMethods.GET, url)
-    .addCredentials(accessToken)
-
-  implicit val system = ActorSystem("QuickStart")
-//  val source: Source[Int, NotUsed] = Source(1 to 2)
-//  val done: Future[Done] = source.runForeach(i => println(i))
-//  done.onComplete(_ => system.terminate())
-
-  implicit val ec = system.dispatcher
-
-  val responseFuture: Future[HttpResponse] = Http().singleRequest(request)
-
-  responseFuture
-    .onComplete {
-      case Success(value) => println("OK --> " + Unmarshal(value.entity).to[String])
-      case Failure(_)   => sys.error("something wrong")
-    }
-
-
-}
-
-//
-
-package akka
-
-import akka.stream.scaladsl._
-
-import scala.concurrent._
-import akka.http.scaladsl.model.headers._
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.unmarshalling.Unmarshal
 import spray.json.DefaultJsonProtocol
-
-import scala.util.{Failure, Success}
-
 
 
 case class Result(id: String)
@@ -85,7 +41,6 @@ object Stream extends App {
   implicit val ec = system.dispatcher
 
 
-
   val ids = List("txn_1Fqdyl2eZvKYlo2Cn0OSmwaY", "txn_1Fqdyi2eZvKYlo2CTca8H4od")
 
   //val source: Source[Int, NotUsed] = Source(1 to 2)
@@ -93,7 +48,7 @@ object Stream extends App {
   val source = Source(ids)
 
   source
-    .runForeach( id => {
+    .runForeach(id => {
       val responseFuture: Future[HttpResponse] = Http().singleRequest(request(id))
       responseFuture.flatMap { r =>
         val body = Unmarshal(r.entity).to[String]
@@ -104,4 +59,5 @@ object Stream extends App {
         println(s"Has-more: ${s.has_more} Last-Id: ${s.data.last.id}")
       }
     }
-  )
+    )
+}
